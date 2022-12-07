e064c51436cd5c2d46ff4ee87cecd6681b915cb1
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-07 17:50:03 +0530

    readme and border radius to button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ export const Home = () => {
         <div style={{ margin: "auto", width: "10%",border:"1px solid black",padding:"3em",marginTop:"5rem" }}>
             <h1 style={{display:"flex",justifyContent:"center"}}>{state.count}</h1>
             <div >
-                <button style={{ marginLeft:"2.7rem",marginBottom: "1rem",backgroundColor:"#CFE2F3",border:"0.2px solid black",padding:"1rem" }} onClick={() => incrementCounter()}>Button</button>
+                <button style={{borderRadius:"20px", marginLeft:"2.7rem",marginBottom: "1rem",backgroundColor:"#CFE2F3",border:"0.2px solid black",padding:"1rem" }} onClick={() => incrementCounter()}>Button</button>
                 <br />
                 <Link to={"/dashboard"}>
-                    <button style={{  marginLeft:"2rem",marginBottom: "1rem",backgroundColor:"#CFE2F3",border:"0.2px solid black",padding:"1rem" }} >Go to Next</button>
+                    <button style={{ borderRadius:"20px", marginLeft:"2rem",marginBottom: "1rem",backgroundColor:"#CFE2F3",border:"0.2px solid black",padding:"1rem" }} >Go to Next</button>
                 </Link>
 
             </div>
